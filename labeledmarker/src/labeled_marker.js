00ae477df6c1c7ced62dd8085e2ebff441aa979d
mikepurvis <mikepurvis@ebafabab-832b-0410-913c-c54cf46817eb>
2007-03-27 18:38:37 +0000

    tabs -> 2 spaces, underscores for private members, moved newEventPassthru inside initialize().

--- labeledmarker/src/labeled_marker.js
+++ labeledmarker/src/labeled_marker.js
@@ -24,84 +24,84 @@
 
 /* Constructor */
 function LabeledMarker(latlng, options){
-    this.latlng = latlng;
-    this.labelText = options.labelText || "";
-    this.labelClass = options.labelClass || "markerLabel";
-    this.labelOffset = options.labelOffset || new GSize(0, 0);
-    
-    this.clickable = options.clickable || true;
-    
-    if (options.draggable) {
-    	// This version of LabeledMarker doesn't support dragging.
-    	options.draggable = false;
-    }
-    
-    GMarker.apply(this, arguments);
+  this.latlng_ = latlng;
+  this.labelText_ = options.labelText || "";
+  this.labelClass_ = options.labelClass || "markerLabel";
+  this.labelOffset_ = options.labelOffset || new GSize(0, 0);
+  
+  this.clickable_ = options.clickable || true;
+  
+  if (options.draggable) {
+  	// This version of LabeledMarker doesn't support dragging.
+  	options.draggable = false;
+  }
+  
+  GMarker.apply(this, arguments);
 }
 
 
-/* It's a limitation of JavaScript inheritance that we can't conveniently
-   extend GMarker without having to run its constructor. In order for the
-   constructor to run, it requires some dummy GLatLng. */
+// It's a limitation of JavaScript inheritance that we can't conveniently
+// extend GMarker without having to run its constructor. In order for the
+// constructor to run, it requires some dummy GLatLng.
 LabeledMarker.prototype = new GMarker(new GLatLng(0, 0));
 
 
 // Creates the text div that goes over the marker.
 LabeledMarker.prototype.initialize = function(map) {
-	// Do the GMarker constructor first.
-	GMarker.prototype.initialize.apply(this, arguments);
-	
-	var div = document.createElement("div");
-	div.className = this.labelClass;
-	div.innerHTML = this.labelText;
-	div.style.position = "absolute";
-	map.getPane(G_MAP_MARKER_PANE).appendChild(div);
+  // Do the GMarker constructor first.
+  GMarker.prototype.initialize.apply(this, arguments);
+  
+  this.map_ = map;
+  this.div_ = document.createElement("div");
+  this.div_.className = this.labelClass_;
+  this.div_.innerHTML = this.labelText_;
+  this.div_.style.position = "absolute";
+  this.div_.style.cursor = "pointer";
+  map.getPane(G_MAP_MARKER_PANE).appendChild(this.div_);
 
-	if (this.clickable) {
-		// Pass through events fired on the text div to the marker.
-		var eventPassthrus = ['click', 'dblclick', 'mousedown', 'mouseup', 'mouseover', 'mouseout'];
-		for(var i = 0; i < eventPassthrus.length; i++) {
-			var name = eventPassthrus[i];
-			GEvent.addDomListener(div, name, newEventPassthru(this, name));
-		}
-
-		// Mouseover behaviour for the cursor.
-		div.style.cursor = "pointer";
-	}
-	
-	this.map = map;
-	this.div = div;
-}
-
-function newEventPassthru(obj, event) {
-	return function() { 
-		GEvent.trigger(obj, event);
-	};
+  if (this.clickable_) {
+    // Creates a closure for passing events through to the source marker
+    // This is located in here to avoid cluttering the global namespace.
+    // The downside is that the local variables from initialize() continue
+    // to occupy space on the stack.
+    function newEventPassthru(obj, event) {
+      return function() { 
+        GEvent.trigger(obj, event);
+      };
+    }
+  
+    // Pass through events fired on the text div to the marker.
+    var eventPassthrus = ['click', 'dblclick', 'mousedown', 'mouseup', 'mouseover', 'mouseout'];
+    for(var i = 0; i < eventPassthrus.length; i++) {
+      var name = eventPassthrus[i];
+      GEvent.addDomListener(this.div_, name, newEventPassthru(this, name));
+    }
+  }
 }
 
 // Redraw the rectangle based on the current projection and zoom level
 LabeledMarker.prototype.redraw = function(force) {
-	GMarker.prototype.redraw.apply(this, arguments);
-	
-	// We only need to do anything if the coordinate system has changed
-	if (!force) return;
-	
-	// Calculate the DIV coordinates of two opposite corners of our bounds to
-	// get the size and position of our rectangle
-	var p = this.map.fromLatLngToDivPixel(this.latlng);
-	var z = GOverlay.getZIndex(this.latlng.lat());
-	
-	// Now position our DIV based on the DIV coordinates of our bounds
-	this.div.style.left = (p.x + this.labelOffset.width) + "px";
-	this.div.style.top = (p.y + this.labelOffset.height) + "px";
-	this.div.style.zIndex = z + 1; // in front of the marker
+  GMarker.prototype.redraw.apply(this, arguments);
+  
+  // We only need to do anything if the coordinate system has changed
+  if (!force) return;
+  
+  // Calculate the DIV coordinates of two opposite corners of our bounds to
+  // get the size and position of our rectangle
+  var p = this.map_.fromLatLngToDivPixel(this.latlng);
+  var z = GOverlay.getZIndex(this.latlng.lat());
+  
+  // Now position our div based on the div coordinates of our bounds
+  this.div_.style.left = (p.x + this.labelOffset.width) + "px";
+  this.div_.style.top = (p.y + this.labelOffset.height) + "px";
+  this.div_.style.zIndex = z; // in front of the marker
 }
 
 // Remove the main DIV from the map pane, destroy event handlers
 LabeledMarker.prototype.remove = function() {
-	GEvent.clearInstanceListeners(this.div);
-	this.div.parentNode.removeChild(this.div);
-	this.div = null;
-	GMarker.prototype.remove.apply(this, arguments);
+  GEvent.clearInstanceListeners(this.div_);
+  this.div_.parentNode.removeChild(this.div_);
+  this.div_ = null;
+  GMarker.prototype.remove.apply(this, arguments);
 }
 
