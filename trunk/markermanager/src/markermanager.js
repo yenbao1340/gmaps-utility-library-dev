9f3e966a0c3babbf580f61271626a6ac59dad7de
pammyla.fox <pammyla.fox@ebafabab-832b-0410-913c-c54cf46817eb>
2008-02-18 19:01:30 +0000

    Adding Icon Factory

--- trunk/markermanager/src/markermanager.js
+++ trunk/markermanager/src/markermanager.js
@@ -1,4 +1,6 @@
-/* Copyright (c) 2007 Google Inc.
+/* 
+ * MarkerManager, v1.0
+ * Copyright (c) 2007 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
