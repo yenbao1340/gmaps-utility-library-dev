277f33ddc5ca65245e35fd57f87420c3efc79687
lemnar <lemnar@ebafabab-832b-0410-913c-c54cf46817eb>
2008-06-17 20:43:35 +0000

    Use the array literal notation [].

--- branches/lemnar-style-branch/extmaptypecontrol/src/extmaptypecontrol.js
+++ branches/lemnar-style-branch/extmaptypecontrol/src/extmaptypecontrol.js
@@ -163,7 +163,7 @@ ExtMapTypeControl.prototype.initialize = function (map) {
 ExtMapTypeControl.prototype.addMapTypeButtons_ = function (map) {
   var me = this;
   var mapTypes = map.getMapTypes();
-  var mapTypeDivs = new [];
+  var mapTypeDivs = [];
   for (var i = 0; i < mapTypes.length; i++) {
     mapTypeDivs[i] = me.createButton_(mapTypes[i].getName());
     mapTypeDivs[i].setAttribute('title', mapTypes[i].getAlt());
